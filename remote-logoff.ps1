db75eea1dc167b6c2efb2440ec3995784e78b863
aeferguson <46054196+aeferguson@users.noreply.github.com>
2018-12-21 11:39:50 -0600

    Create remote-logoff.ps1

---
+++ remote-logoff.ps1
@@ -0,0 +1,29 @@
+﻿# A script for prompting for a server and username and then trying steps to log that user out of the server.
+
+Param (
+
+    [parameter(ValueFromPipeline=$True)]
+    [string[]]$ComputerName,$UserName
+
+)
+<#
+$ComputerName = Read-Host "Please enter a server"
+$UserName = Read-Host "Please enter a username"
+#>
+
+$SessionId = ((quser /server:$ComputerName | Where-Object { $_ -match $UserName }) -split ' +')[3]
+<#
+
+The above command queries the server which was input and returns the session ID
+You can return other values such as Session Name, State, Idle Time, and Logon Time
+
+#>
+
+Write-host "User '$UserName' will be logged off, are you sure you want to proceed?"
+$ynResult = Read-Host "y / n"
+Switch($ynResult)
+{
+    Y {Write-Host "Attempting to logoff '$UserName'"; logoff $SessionId /server:$ComputerName}
+    N {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
+    Default {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
+}
