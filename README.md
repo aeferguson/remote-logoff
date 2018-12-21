4648425d0e2f21dd5e13d9ede7497ea9871123b7
aeferguson <46054196+aeferguson@users.noreply.github.com>
2018-12-21 12:49:20 -0600

    error catching, more readme
    -added some clumsy error catching in script
    -added descriptions in readme

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Remote-Logoff
 
-This PowerShell script queries a remote computer for the session ID of a particular user.
-It then attempts to log that user off of the computer.
+This PowerShell script queries a remote computer for the session ID of a particular user and attempts to log that user off.
+This script will accept a hostname or an IP address as the 'ComputerName'.
+
 
 ## Examples 
 Log a user 'Admin-Bob' off of a Windows machine 'Server42'
 ```
-.\remote-logoff.ps1 -ComputerName "Server42" -UserName "Admin-Bob"
+.\remote-logoff.ps1 -ComputerName Server42 -UserName Admin-Bob
 ```
 ### Prerequisites
-You will need administrator credentials to the remote machine and to be on the same domain (if applicable)
+You will need administrator credentials to the remote machine and to be on the same domain (if applicable).
+
 
 ## Authors
 
--- remote-logoff.ps1
+++ remote-logoff.ps1
@@ -1,25 +1,26 @@
 ﻿# A script for prompting for a server and username and then trying steps to log that user out of the server.
-
 Param (
 
-    [parameter(ValueFromPipeline=$True)]
-    [string[]]$ComputerName,$UserName
+        [parameter(ValueFromPipeline=$True)]
+        [string[]]$ComputerName,$UserName
 
-)
-
-$SessionId = ((quser /server:$ComputerName | Where-Object { $_ -match $UserName }) -split ' +')[3]
-<#
-
-The above command queries the server which was input and returns the session ID
-You can return other values such as Session Name, State, Idle Time, and Logon Time
-
-#>
-
-Write-host "User '$UserName' will be logged off, are you sure you want to proceed?"
-$ynResult = Read-Host "y / n"
-Switch($ynResult)
+    )
+$ErrorActionPreference = 'Stop';
+try
 {
-    Y {Write-Host "Attempting to logoff '$UserName'"; logoff $SessionId /server:$ComputerName}
-    N {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
-    Default {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
+  $SessionId = ((quser /server:$ComputerName | Where-Object { $_ -match $UserName }) -split ' +')[3]
+  Write-host "User '$UserName' will be logged off, are you sure you want to proceed?"
+  $ynResult = Read-Host "y / n"
+  Switch($ynResult)
+    {
+        Y {Write-Host "Attempting to logoff '$UserName'"; logoff $SessionId /server:$ComputerName}
+        N {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
+        Default {Write-Host "Exiting doing nothing..."; Start-Sleep -s 1}
+    }
 }
+catch
+{
+  Write-Warning 'Computer offline or not responding'
+}
+
+    
